0cacbca5c08c441e5f46a8c316d6d6c6767f7e14
Cyber Squib <peterhodgden@icloud.com>
2026-09-03 07:11:29 -0500

    implemented player spritesheet

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 function love.load()
 
+  _gfx={}
+
+  table.insert(_gfx,love.graphics.newImage("resource/sliceAll_KappaDance.png"))
+
   love.window.setMode(960,720)
 
 end
@@ -7,5 +11,7 @@ end
 function love.draw()
 
   love.graphics.clear(.5,.5,.5,1)
+  
+    love.graphics.draw(_gfx[1],8,8)--,0,1.25,1.25)
 
 end
